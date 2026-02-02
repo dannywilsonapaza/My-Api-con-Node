13e6cf58a23aec3a9b37ce80dc2c5c77aae85f70
Danny <dannyapaza40@gmail.com>
2026-02-02 10:56:00 -0500

    test metodo post

--- test/e2e-test.test.js
+++ test/e2e-test.test.js
@@ -1,20 +1,46 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const expect = require('chai').expect;
+
 chai.use(chaiHttp);
 
+const url = 'http://localhost:3000';
 
-const app = require('../index').app;
 
-describe('Suite de pruebas e2e para el curso', ()=>{
-    it('esperamos un hola mundo', (done)=>{
-        chai.request('http://localhost:3000')
-        .get('/')
+
+describe('Inserte el nombre y el precio', ()=>{
+    it('Esperamos un name y un price', (done)=>{
+        chai.request(url)
+        .post('/api/v1/products')
+        .send({name: 'Compu', price: 2000})
         .end((err,res)=>{
-            console.log('A')
-            chai.assert.equal(res.text, 'Hola Mundo');
-            done()
+            console.log(res.body);
+            expect(res).to.have.status(200);
+            done();
         })
-        console.log('B')
     })
+})
 
-    })
+
+
+
+
+
+
+
+
+
+
+// describe('Suite de pruebas e2e para el curso', ()=>{
+//     it('esperamos un hola mundo', (done)=>{
+//         chai.request('http://localhost:3000')
+//         .get('/')
+//         .end((err,res)=>{
+//             console.log('A')
+//             chai.assert.equal(res.text, 'Hola Mundo');
+//             done()
+//         })
+//         console.log('B')
+//     })
+
+//     })
